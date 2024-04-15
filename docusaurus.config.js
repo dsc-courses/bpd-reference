6cdc9a3ae4e59ccc364c9ab8f1273c00c239d649
m1nce <miinchaankiim@gmail.com>
2024-04-15 03:23:43 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://m1nce.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bpd-reference/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dsc10', // Usually your GitHub org/user name.
+  organizationName: 'm1nce', // Usually your GitHub org/user name.
   projectName: 'bpd-reference', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/m1nce/bpd-reference',
         },
         theme: {
           customCss: './src/css/custom.css',
