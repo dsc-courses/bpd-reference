eb52edcb8f75b77de9f536ee3381e28795d1a84a
m1nce <miinchaankiim@gmail.com>
2024-04-17 02:45:06 -0700

    dark mode works as intended

--- components/DataFrameComponent.jsx
+++ components/DataFrameComponent.jsx
@@ -1,37 +1,44 @@
-// DataFrameComponent.js
+// DataFrameComponent.jsx
 import React from 'react';
 import '../src/css/dataframe-styles.css';
 
 const DataFrameComponent = ({ data }) => {
   let columns = [];
   let rows = [];
+  let indexName = "";
+  let indexValues = [];
 
-  // Only attempt to parse `data` if it's not undefined
   if (data) {
     const parsedData = JSON.parse(data);
     columns = parsedData.columns || [];
     rows = parsedData.data || [];
+    indexName = parsedData.indexName || "Index"; // Use the provided index name
+    indexValues = parsedData.index || []; // The index values
   }
 
   return (
-    <table className="dataframe-table">
-      <thead>
-        <tr>
-          {columns.map((col, index) => (
-            <th key={index}>{col}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <td key={cellIndex}>{cell}</td>
+    <div className='dataframe-container'>
+      <table className="dataframe-table">
+        <thead>
+          <tr>
+            <th>{indexName}</th> {/* Render the index name */}
+            {columns.map((col, index) => (
+              <th key={index}>{col}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {rows.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <td>{indexValues[rowIndex]}</td> {/* Render index values */}
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
